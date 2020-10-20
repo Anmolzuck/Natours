7a8847693b663f9f7f3a1eac3abcdc4c81f4fbae
Anmol Zakie <anmolzuck17@gmail.com>
2020-10-20 12:34:58 +0530

    Update

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,9 +51,9 @@ exports.signup = catchAsync(async (req, res, next) => {
   });
 
   //Sending email
-  //const url = `${req.protocol}://${req.get('host')}/me`;
+  const url = `${req.protocol}://${req.get('host')}/me`;
   //console.log(url);
-  //await new Email(newUser, url).sendWelcome();
+  await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
   //Creating a token
