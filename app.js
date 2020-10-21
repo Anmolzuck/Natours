081fd014858baec38f7b270fccba9211808cba96
Anmol Zakie <anmolzuck17@gmail.com>
2020-10-21 12:26:18 +0530

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -23,6 +24,12 @@ app.enable('trust proxy');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+//Implement cors
+app.use(cors());
+//Acess-Control-Allow-Origin
+
+app.options('*', cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
