84020d54784def024950d93c33925ed727a1b514
Anmol Zakie <anmolzuck17@gmail.com>
2020-10-22 10:38:24 +0530

    Back to old

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -5,43 +5,19 @@ const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handleFactory');
 
-// exports.getCheckoutSession = catchAsync(async (req, res, next) => {
-//   // 1) Get the currently booked tour
-//   const tour = await Tour.findById(req.params.tourId);
-//   // console.log(tour);
-
-//   // 2) Create checkout session
-//   const session = await stripe.checkout.sessions.create({
-//     payment_method_types: ['card'],
-//     success_url: `${req.protocol}://${req.get('host')}/my-tours`,
-//     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
-//     customer_email: req.user.email,
-//     client_reference_id: req.params.tourId,
-//     display_items: [
-//       {
-//         name: `${tour.name} Tour`,
-//         description: tour.summary,
-//         images: [
-//           `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
-//         ],
-//         amount: tour.price * 100,
-//         currency: 'usd',
-//         quantity: 1,
-//       },
-//     ],
-//   });
-
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
-  //1 Get the currently booked tour based on tour id
+  // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourID);
+  // console.log(tour);
 
+  // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     success_url: `${req.protocol}://${req.get('host')}/my-tours`,
-    cancel_url: `${req.protocol}://${req.get('host')}/tours/${tour.slug}`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourID,
-    line_items: [
+    display_items: [
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
@@ -54,6 +30,29 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       },
     ],
   });
+
+  //  exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  // //1 Get the currently booked tour based on tour id
+  //  const tour = await Tour.findById(req.params.tourID);
+
+  // const session = await stripe.checkout.sessions.create({
+  //    payment_method_types: ['card'],
+  //    success_url: `${req.protocol}://${req.get('host')}/my-tour`,
+  //   cancel_url: `${req.protocol}://${req.get('host')}/tours/${tour.slug}`,
+  //   customer_email: req.user.email,
+  //   client_reference_id: req.params.tourID,
+  //   line_items: [
+
+  //     {
+  //       name: `${tour.name} Tour`,
+  //       description: tour.summary,
+  //       images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
+  //       amount: tour.price * 100,
+  //       currency: 'usd',
+  //       quantity: 1,
+  //     },
+  //   ],
+  // });
 
   // 3) Create session as response
   res.status(200).json({
