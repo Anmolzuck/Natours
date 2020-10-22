4fcd558b7cfd8eca704c099c5f411ef56def3964
Anmol Zakie <anmolzuck17@gmail.com>
2020-10-22 09:25:55 +0530

    Back to old

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -66,6 +66,7 @@ exports.webhookCheckout = (req, res, next) => {
     createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
+  next();
 };
 
 exports.createBooking = factory.createOne(Booking);
